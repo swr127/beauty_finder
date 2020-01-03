cca67b46970e8ac8ef3395160e9e3e792cc21519
swr127 <samantha.w.russell@gmail.com>
2020-01-03 13:48:39 -0500

    rendering articles

--- js/main.js
+++ js/main.js
@@ -19,11 +19,27 @@ const renderArticles = async () => {
     console.log(responseArticles)
 
     const articleDetails = document.querySelector('article')
-        articleDetails.innerHTML = ''
+        articleDetails.innerHTML = '' 
 
-    const publishedDate = document.createElement('p')
-        publishedDate.innerHTML = `PUBLISHED: ${responseArticles.data.articles[0].publishedAt}`
-        articleDetails.append(publishedDate)
+    const articleDate = document.createElement('p')
+        articleDate.innerHTML = `PUBLISHED: ${responseArticles.data.articles[0].publishedAt}`
+        articleDetails.append(articleDate)
+
+    const articleSource = document.createElement('p')
+        articleSource.innerHTML = `SOURCE: ${responseArticles.data.articles[0].source.name}`
+        articleDetails.append(articleSource)
+    
+    const articleAuthor = document.createElement('p')
+        articleAuthor.innerHTML = `AUTHOR: ${responseArticles.data.articles[0].author}`
+        articleDetails.append(articleAuthor)
+    
+    const articleTitle = document.createElement('p')
+        articleTitle.innerHTML = `TITLE: ${responseArticles.data.articles[0].title}`
+        articleDetails.append(articleTitle)
+    
+    const articleDescription = document.createElement('p')
+        articleDescription.innerHTML = `${responseArticles.data.articles[0].content}`
+        articleDetails.append(articleDescription)
        
 }
 
