d9fbfbee7662532bb4ca820026b0c823e3b244de
swr127 <samantha.w.russell@gmail.com>
2020-01-03 16:29:56 -0500

    updated ForEach() function

--- js/main.js
+++ js/main.js
@@ -22,45 +22,20 @@ const renderArticles = async () => {
 
     const articleDetails = document.querySelector('section')
     articleDetails.innerHTML = ''
+
     articles.forEach(article => {
         const newArticle = document.createElement('article')
         newArticle.innerHTML =  `
         <img src=${article.urlToImage} />
         <p>PUBLISHED: ${article.publishedAt}</p>
         <p>SOURCE: ${article.source.name}</p>
+        <p>AUTHOR: ${article.author}</p>
+        <p>TITLE: ${article.title}</p>
+        <p>${article.content}</p>
         `
         articleDetails.append(newArticle)
 
     })
-
-    // for (let i = 0; i <= articles.length; i++) {
-
-    //     const articleDetails = document.querySelector('article')
-    //         articleDetails.innerHTML = '' 
-    
-    //     const articleImage = document.querySelector('article')
-    //         articleDetails.innerHTML = `<img src='${articles[i].urlToImage}'/>`
-
-    //     const articleDate = document.createElement('p')
-    //         articleDate.innerHTML = `PUBLISHED: ${articles[i].publishedAt}`
-    //         articleDetails.append(articleDate)
-
-    //     const articleSource = document.createElement('p')
-    //         articleSource.innerHTML = `SOURCE: ${articles[i].source.name}`
-    //         articleDetails.append(articleSource)
-    
-    //     const articleAuthor = document.createElement('p')
-    //         articleAuthor.innerHTML = `AUTHOR: ${articles[i].author}`
-    //         articleDetails.append(articleAuthor)
-    
-    //     const articleTitle = document.createElement('p')
-    //         articleTitle.innerHTML = `TITLE: ${articles[i].title}`
-    //         articleDetails.append(articleTitle)
-    
-    //     const articleDescription = document.createElement('p')
-    //         articleDescription.innerHTML = `${articles[i].content}`
-    //         articleDetails.append(articleDescription)
-    // }
 }
 
 renderArticles()
